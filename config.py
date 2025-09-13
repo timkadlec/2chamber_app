92be4ba5b1fd4fc896d8e5296d4604d949200c7c
timkadlec <tim@mwq.cz>
2025-09-13 17:17:51 +0200

    construction of connection string of dbs

--- config.py
+++ config.py
@@ -11,6 +11,18 @@ def construct_oracle_db_uri(user, password, host, port, service_name):
     return f"oracle+oracledb://{user}:{password}@{host}:{port}/?service_name={service_name}"
 
 
+def construct_sqlite_db_uri(db_file):
+    return f"sqlite:///{db_file}"
+
+
+def construct_mysql_db_uri(user, password, host, port, db_name):
+    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+def construct_postgres_db_uri(user, password, host, port, db_name):
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -27,10 +39,22 @@ class BaseConfig:
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = construct_postgres_db_uri(
+        user=os.environ.get('POSTGRES_DB_USER'),
+        password=os.environ.get('POSTGRES_DB_PSWD'),
+        host=os.environ.get('POSTGRES_DB_HOST'),
+        port=os.environ.get('POSTGRES_DB_PORT'),
+        db_name=os.environ.get('POSTGRES_DB_NAME')
+    )
     DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = construct_postgres_db_uri(
+        user=os.environ.get('POSTGRES_DB_USER'),
+        password=os.environ.get('POSTGRES_DB_PSWD'),
+        host=os.environ.get('POSTGRES_DB_HOST'),
+        port=os.environ.get('POSTGRES_DB_PORT'),
+        db_name=os.environ.get('POSTGRES_DB_NAME')
+    )
     DEBUG = False
